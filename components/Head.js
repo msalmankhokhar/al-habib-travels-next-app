ff1f7ae33fd1781b3bbaff0799056f8358ba5f4b
msalmankhokhar <kjokhars@gmail.com>
2024-12-05 21:54:49 +0500

    added facebook-domain-verification meta tag

--- components/Head.js
+++ components/Head.js
@@ -8,6 +8,7 @@ export default function Head({ title, desc, imgSrc="/img/ogimage1.png" }) {
             <meta name="description" content={desc} />
             <meta property="og:image" content={`https://al-habib-travels-next-app.vercel.app${imgSrc}`} />
             <meta name="google-site-verification" content="I2jsNVBKwvp_5Sx1k7-6X8JEj7sI_OqZEUnNOqXaxrc" />
+            <meta name="facebook-domain-verification" content="2atcfhak55w5ehq9wjv41r05n9o46b" />
         </NextHead>
     )
 }
