9f5a0981d05cd33ff0bc755ce1a3d71301bda12b
msalmankhokhar <kjokhars@gmail.com>
2024-12-08 16:04:56 +0500

    added whatsapp floating button

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { Poppins as Font } from "next/font/google";
 import useScrollRestoration from "@/hooks/useScrollRestroration";
 import { PagesTopLoader } from 'nextjs-toploader/pages'
 import { GoogleTagManager } from '@next/third-parties/google'
+import { FloatingWhatsApp } from "react-floating-whatsapp";
 
 const font = Font({
   weight: ['300', '400', '500', '600', '700', '800'],
@@ -23,6 +24,12 @@ export default function App({ Component, pageProps }) {
     showSpinner={false}
     />
     <Component {...pageProps} />
+    <FloatingWhatsApp 
+      phoneNumber="+44203 504 2344"
+      accountName="Al Habib Travel"
+      avatar="/img/logos/favicon.png"
+      buttonStyle={{width: '45px', height: '45px'}}
+    />
     <GoogleTagManager gtmId="GTM-KGQFSMNM" />
   </>
 }
